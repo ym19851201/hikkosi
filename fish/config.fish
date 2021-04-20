81626762e5979c31d704d5093b6125458458c32f
Yohei Miyazaki <miyazaki19851201@gmail.com>
2021-04-20 23:38:43 +0900

    1st commit

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+sudo mount -i -o remount,exec /home/chronos/user/
+
+#export PYENV_ROOT="/usr/local/bin/.pyenv"
+#set -x PATH $PYENV_ROOT/bin $PATH
+#set -x TERM xterm-256color
+#. (pyenv init - | psub)
+#cd
+
+# added by Miniconda3 3.16.0 installer
+#set -x PATH /home/chronos/user/miniconda3/bin $PATH
+alias chrome '/bin/bash /usr/local/share/nodebrew/node/v10.10.0/lib/node_modules/chromium/lib/chromium/chrome-linux/chrome'
+set -x PATH $HOME/.nodebrew/current/bin $PATH
+
+set -x TMPDIR /usr/local/tmp
+#set -x PATH $HOME/go/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
